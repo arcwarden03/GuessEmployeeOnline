e94abd49e5e4c0c49c625d90d35860c9cfdeb34a
arcwarden03 <blankneverlose0709@gmail.com>
2019-07-03 17:38:09 +0800

    initial commit

---
+++ access-denied.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en-US">
+<head>
+	<title>Diversion Industries Inc. - Log In</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link rel='stylesheet' id='login-css'  href='../../../Diversion/Login/dept/cci/rf/login.css' type='text/css' media='all' />
+<link rel='stylesheet' id='colors-fresh-css'  href='../../../Diversion/Login/dept/cci/rf/colors-fresh.css' type='text/css' media='all' />
+<meta name='robots' content='noindex,nofollow' />
+</head>
+<body class="login">
+<div id="login">
+<br>
+<p class="message" align="center">Access Denied!<br />
+  You do not have access to this resource. <br />
+  Please try to <a href="../../Index.php"> login </a>first!
+  <br></p>
+ </div>
+</body>
+</html>
